25664ca8fdd18edd703944778cae31e4326ba7f5
hasti-15 <hasti.ds@somaiya.edu>
2020-11-24 11:38:58 +0530

    mark_attendance

--- lib/screens/record_attendance.dart
+++ lib/screens/record_attendance.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 // import "appbar.dart";
 // import 'package:flutter/services.dart';
@@ -84,6 +85,80 @@ class _MarkState extends State<MarkAttendance> {
             "${place.locality}, ${place.postalCode}, ${place.country}";
       });
       print(_currentAddress);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
+
+
+*/
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MarkAttendance extends StatefulWidget {
+  @override
+  _MarkState createState() => _MarkState();
+
+}
+class _MarkState extends State<MarkAttendance> {
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+  Position _currentPosition;
+  String _currentAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Location"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_currentPosition != null) Text(_currentAddress ?? 'mumbai'),
+            FlatButton(
+              child: Text("Get location"),
+              onPressed: () {
+                _getCurrentLocation();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+
+      _getAddressFromLatLng();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = p[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
     } catch (e) {
       print(e);
     }
