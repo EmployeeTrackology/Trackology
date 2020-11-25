b3a6c91b0f56c55cda7bf63c861692b0c05a7eb0
hasti-15 <hasti.ds@somaiya.edu>
2020-11-25 10:55:28 +0530

    --- Auto-Git Commit ---

--- lib/screens/record_attendance.dart
+++ lib/screens/record_attendance.dart
@@ -1,101 +1,5 @@
+
 /*
-import 'package:flutter/material.dart';
-// import "appbar.dart";
-// import 'package:flutter/services.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart';
-
-class Location {
-  double latti, longi;
-  Future<void> grab() async {
-    try {
-      Position pos = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      latti = pos.latitude;
-      longi = pos.longitude;
-      print(latti);
-      print(longi);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  Location l = Location();
-}
-
-class MarkAttendance extends StatefulWidget {
-  @override
-  _MarkState createState() => _MarkState();
-}
-
-class _MarkState extends State<MarkAttendance> {
-  final Geolocator geolocator = Geolocator();
-
-  Position _currentPosition;
-  String _currentAddress;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Location"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // if (_currentPosition != null) Text(_currentAddress),
-            RaisedButton(
-              child: Text("Get location"),
-              onPressed: () {
-                _getCurrentLocation();
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _getCurrentLocation() {
-    Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-        .then((Position position) {
-      print("fjhfjh");
-      setState(() {
-        _currentPosition = position;
-        print(_currentPosition);
-      });
-
-      _getAddressFromLatLng();
-    }).catchError((e) {
-      print("fjhfjh");
-      print(e);
-    });
-  }
-
-  _getAddressFromLatLng() async {
-    try {
-      List<Placemark> p = await placemarkFromCoordinates(
-          _currentPosition.latitude, _currentPosition.longitude);
-
-      Placemark place = p[0];
-
-      setState(() {
-        _currentAddress =
-            "${place.locality}, ${place.postalCode}, ${place.country}";
-      });
-      print(_currentAddress);
-    } catch (e) {
-      print(e);
-    }
-  }
-}
-
-
-
-*/
-
-
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
@@ -164,3 +68,53 @@ class _MarkState extends State<MarkAttendance> {
     }
   }
 }
+*/
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:emp_tracker/screens/appbar.dart';
+
+class MarkAttendance extends StatefulWidget {
+  @override
+  _MarkState createState() => _MarkState();
+
+}
+class _MarkState extends State<MarkAttendance>  {
+
+  String _locationMessage = ""; 
+
+  void _getCurrentLocation() async {
+
+    final position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+
+    setState(() {
+      _locationMessage = "latitude =${position.latitude},longitude =${position.longitude}";
+    });
+  }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mark Attendance',
+      home: Scaffold(
+        appBar: new MyAppBar("Mark Attendance"),
+        body: Align(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            Text(_locationMessage), 
+            FlatButton(
+              onPressed: () {
+                  _getCurrentLocation();
+              },
+              color: Colors.green,
+              child: Text("Find Location")
+            )
+          ]),
+        )
+      )
+    );
+  }
+}
+
