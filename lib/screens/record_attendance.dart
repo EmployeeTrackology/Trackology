c0ad3edfbb9765084d603a6d80ca3daac77d494b
hasti-15 <hasti.ds@somaiya.edu>
2020-11-25 12:44:32 +0530

    --- Auto-Git Commit ---

--- lib/screens/record_attendance.dart
+++ lib/screens/record_attendance.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:emp_tracker/screens/appbar.dart';
@@ -35,6 +36,7 @@ class _MarkState extends State<MarkAttendance>  {
             FlatButton(
               onPressed: () {
                   _getCurrentLocation();
+                  
               },
               color: Colors.green,
               child: Text("Find Location")
@@ -45,4 +47,59 @@ class _MarkState extends State<MarkAttendance>  {
     );
   }
 }
+*/
 
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:emp_tracker/screens/appbar.dart';
+
+class MarkAttendance extends StatefulWidget {
+  @override
+  _MarkState createState() => _MarkState();
+
+}
+
+class _MarkState extends State<MarkAttendance>  {
+  Position _currentPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new MyAppBar("Mark Attendance"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_currentPosition != null)
+              Text(
+                  "LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}"),
+            FlatButton(
+              child: Text("Get location"),
+              onPressed: () {
+                _getCurrentLocation();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getCurrentLocation() {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+}
