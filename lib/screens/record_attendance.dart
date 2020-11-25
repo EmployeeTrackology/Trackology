a08df66176ba68a1acf0062aa89adecaf82847ee
hiral72 <46996903+hiral72@users.noreply.github.com>
2020-11-25 14:05:30 +0530

    minor change

--- lib/screens/record_attendance.dart
+++ lib/screens/record_attendance.dart
@@ -1,32 +1,87 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:emp_tracker/screens/appbar.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:geolocator/geolocator.dart';
+// import 'package:emp_tracker/screens/appbar.dart';
 
+// class MarkAttendance extends StatefulWidget {
+//   @override
+//   _MarkState createState() => _MarkState();
+
+// }
+
+// class _MarkState extends State<MarkAttendance>  {
+//   Position _currentPosition;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: new MyAppBar("Mark Attendance"),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             if (_currentPosition != null)
+//               Text(
+//                   "LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}"),
+//             FlatButton(
+//               child: Text("Get location"),
+//               onPressed: () {
+//                 _getCurrentLocation();
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   _getCurrentLocation() {
+//     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+//     geolocator
+//         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+//         .then((Position position) {
+//       setState(() {
+//         _currentPosition = position;
+//       });
+//     }).catchError((e) {
+//       print(e);
+//     });
+//   }
+// }
+import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+ import 'package:emp_tracker/screens/appbar.dart';
 class MarkAttendance extends StatefulWidget {
   @override
   _MarkState createState() => _MarkState();
-
 }
-
-class _MarkState extends State<MarkAttendance>  {
+class _MarkState extends State<MarkAttendance> {
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
   Position _currentPosition;
-
+  String _currentAddress;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new MyAppBar("Mark Attendance"),
+     appBar: new MyAppBar("Mark Attendance"),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            if (_currentPosition != null)
-              Text(
-                  "LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}"),
-            FlatButton(
-              child: Text("Get location"),
+            if (_currentPosition != null) Text(_currentAddress ?? 'mumbai'),
+            RaisedButton(
+              color:Colors.green,
+              child: Text("IN:Get location"),
               onPressed: () {
-                _getCurrentLocation();
+                _getCurrentLocation('in');
+              },
+            ),
+            RaisedButton(
+              color:Colors.red,
+              child: Text("OUT:Get location"),
+              onPressed: () {
+                _getCurrentLocation('out');
               },
             ),
           ],
@@ -34,18 +89,33 @@ class _MarkState extends State<MarkAttendance>  {
       ),
     );
   }
-
-  _getCurrentLocation() {
-    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-
+  _getCurrentLocation(String x) {
     geolocator
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-        .then((Position position) {
+        .then((Position position) {    
       setState(() {
         _currentPosition = position;
       });
+      _getAddressFromLatLng();
     }).catchError((e) {
       print(e);
     });
   }
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+      Placemark place = p[0];
+      setState(() {
+        print(_currentAddress);
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+            print(_currentAddress);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 }
+
+
